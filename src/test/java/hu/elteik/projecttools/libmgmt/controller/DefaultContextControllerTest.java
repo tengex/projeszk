85c509eaf9f3ba89b2a14575defa53c6773062c7
aron51 <aron.szabo51@gmail.com>
2017-05-23 01:29:21 +0200

    main tests added

---
+++ src/test/java/hu/elteik/projecttools/libmgmt/controller/DefaultContextControllerTest.java
@@ -0,0 +1,7 @@
+package hu.elteik.projecttools.libmgmt.controller;
+
+/**
+ * Created by aron51 on 2017. 05. 22..
+ */
+public class DefaultContextControllerTest {
+}
---
+++ src/test/java/hu/elteik/projecttools/libmgmt/service/MainServiceTest.java
@@ -0,0 +1,7 @@
+package hu.elteik.projecttools.libmgmt.service;
+
+/**
+ * Created by aron51 on 2017. 05. 22..
+ */
+public class MainServiceTest {
+}
